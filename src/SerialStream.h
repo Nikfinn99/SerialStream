69058de7058fa93cd188d1f95182a8906b99070c
Nikfinn99 <43323055+Nikfinn99@users.noreply.github.com>
2019-03-25 11:36:02 +0100

    add stream helpers

--- src/SerialStream.h
+++ src/SerialStream.h
@@ -2,6 +2,10 @@
 #define Serial_Stream_H_
 #include <Print.h>
 
+#define endl "\n"
+#define space " "
+#define tab "\t"
+
 template<class T> inline Print &operator <<(Print &obj, T arg) { obj.print(arg); return obj; }
 
 #endif //Serial_Stream_H_
